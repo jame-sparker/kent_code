526a3220c67509fd362e87e90fb989677c6d8c05
James Parker <jsparker512@gmail.com>
2016-06-25 22:30:10 +0900

    Fixed a syntax error and adjusted delay time

--- sound_module/sound_module.ino
+++ sound_module/sound_module.ino
@@ -1,6 +1,7 @@
 const int KNOCK_PIN = 0;
 const int MIN_KNOCK = 10;
-const char[] KNOCK_MESSAGE = "KNOCK"; 
+const char KNOCK_MESSAGE[] = "KNOCK"; 
+const int DELAY_TIME = 300; //delay for knock 
 
 int knock = 0;
 
@@ -14,7 +15,7 @@ void loop() {
     
   if(knock > MIN_KNOCK){
     Serial.print(KNOCK_MESSAGE); 
-    delay(500);
+    delay(DELAY_TIME);
   }
   
 }
