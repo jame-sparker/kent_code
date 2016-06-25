7add1d5be23d98cb63e8dbcc9c466dff50770754
theworldbright <kent@kentshikama.com>
2016-06-25 14:28:34 +0900

    Replace with bluetooth module code

--- sound_module/sound_module.ino
+++ sound_module/sound_module.ino
@@ -1,16 +1,9 @@
-int knock = 0;
-
 void setup() {
-  Serial.begin(38400);
-}
+  Serial.begin(9600); 
+} 
 
-void loop() {
-  knock = analogRead(0);
-  if (knock > 10) {
-    Serial.println(knock);
-    digitalWrite(13,HIGH);
-    delay(500);
-    digitalWrite(13,LOW);
-  }
+void loop() { 
+  Serial.println("test"); 
+  delay(1000); 
 }
 
