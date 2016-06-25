0b5ae732e187b584ee1241b30c95186e631e795c
James Parker <jsparker512@gmail.com>
2016-06-25 22:18:00 +0900

    Updating Sound Module Sound module is working and tested with linux. baud number was issue. The spec for HC-06 said 9600 was the default.

--- sound_module/sound_module.ino
+++ sound_module/sound_module.ino
@@ -1,9 +1,21 @@
+const int KNOCK_PIN = 0;
+const int MIN_KNOCK = 10;
+const char[] KNOCK_MESSAGE = "KNOCK"; 
+
+int knock = 0;
+
 void setup() {
   Serial.begin(9600); 
 } 
 
 void loop() { 
-  Serial.println("test"); 
-  delay(1000); 
+
+  knock = analogRead(KNOCK_PIN);
+    
+  if(knock > MIN_KNOCK){
+    Serial.print(KNOCK_MESSAGE); 
+    delay(500);
+  }
+  
 }
 
